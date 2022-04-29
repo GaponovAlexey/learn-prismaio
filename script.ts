1e70e09c5488f3d2fe0838ce198d96d3b2f3a196
Kekelberi <alexei.gaponov@gmail.com>
2022-04-29 21:09:18 +0300

    save

---
+++ script.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+// A `main` function so that you can use async/await
+async function main() {
+  const result = await prisma.user.findMany()
+  console.log(result);
+  
+  
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
